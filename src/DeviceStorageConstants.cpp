b8ed998d7ad23da48a127695be7debfa0e9376c3
bdkazal <bdkazal@gmail.com>
2026-06-17 14:33:12 +0600

    Define storage constant members for C++11 builds

---
+++ src/DeviceStorageConstants.cpp
@@ -0,0 +1,6 @@
+#include "DeviceStorage.h"
+
+constexpr const char *DeviceStorage::StorageNamespace;
+constexpr const char *DeviceStorage::WifiSsidKey;
+constexpr const char *DeviceStorage::WifiPasswordKey;
+constexpr const char *DeviceStorage::ProvisioningKey;

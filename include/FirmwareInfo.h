b820df4aeaebeb668a39a2fa5d974aab1453a2fd
bdkazal <bdkazal@gmail.com>
2026-06-17 12:56:54 +0600

    Bump firmware version for interactive console

--- include/FirmwareInfo.h
+++ include/FirmwareInfo.h
@@ -4,5 +4,5 @@ namespace FirmwareInfo
 {
 constexpr const char *ProductName = "Biztola Smart Fountain";
 constexpr const char *BoardName = "ESP32-WROOM-32";
-constexpr const char *Version = "smart-fountain-32s-foundation-0.3-gpio-test";
+constexpr const char *Version = "smart-fountain-32s-foundation-0.4-interactive-console";
 }

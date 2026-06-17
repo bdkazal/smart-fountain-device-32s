e7702c68df2fd00db52715292bfe149d97082c6a
bdkazal <bdkazal@gmail.com>
2026-06-17 12:48:31 +0600

    Add controlled output signal validation commands

--- include/HardwareValidator.h
+++ include/HardwareValidator.h
@@ -20,13 +20,26 @@ private:
         White
     };
 
+    enum class OutputPulse : uint8_t
+    {
+        None,
+        Pump,
+        Cob
+    };
+
     NeoPixelStep currentStep = NeoPixelStep::Off;
+    OutputPulse activePulse = OutputPulse::None;
     unsigned long lastStepAt = 0;
+    unsigned long pulseStartedAt = 0;
     bool started = false;
 
     static constexpr unsigned long StepIntervalMs = 3000;
+    static constexpr unsigned long OutputPulseDurationMs = 2000;
     static constexpr uint8_t TestLevel = 32;
 
     void applyCurrentStep(HardwareOutputs &outputs);
     void advanceStep();
+    void handleSerialCommands(HardwareOutputs &outputs);
+    void startOutputPulse(OutputPulse pulse, HardwareOutputs &outputs);
+    void stopOutputPulse(HardwareOutputs &outputs);
 };

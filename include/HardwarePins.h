1e8bb1f59a1dc91e67cf44e66ddc081317692980
bdkazal <bdkazal@gmail.com>
2026-06-17 13:20:31 +0600

    Add local pump and COB button pins

--- include/HardwarePins.h
+++ include/HardwarePins.h
@@ -7,6 +7,8 @@
 
 namespace HardwarePins
 {
+constexpr gpio_num_t PumpButton = GPIO_NUM_18;
+constexpr gpio_num_t CobButton = GPIO_NUM_19;
 constexpr gpio_num_t PumpOutput = GPIO_NUM_25;
 constexpr gpio_num_t CobOutput = GPIO_NUM_26;
 constexpr gpio_num_t NeoPixelData = GPIO_NUM_27;
@@ -16,8 +18,10 @@ constexpr gpio_num_t WifiResetButton = GPIO_NUM_33;
 constexpr bool PumpActiveHigh = true;
 constexpr bool CobActiveHigh = true;
 constexpr bool WaterLevelActiveLow = true;
+constexpr bool LocalButtonsActiveLow = true;
 
 constexpr uint16_t NeoPixelCount = 30;
 constexpr uint8_t NeoPixelMaxBrightness = 160;
 constexpr unsigned long WaterLevelDebounceMs = 250;
+constexpr unsigned long LocalButtonDebounceMs = 50;
 }
